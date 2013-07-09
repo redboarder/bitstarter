bc25b630b951cf4c6ce5cc80a7dc283d02a29b09
Kirill <k.gorbushin@gmail.com>
2013-07-09 23:36:15 +0200

    updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,20 @@
+#!/usr/bin/env node
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
+
+var content;
+// First I want to read the file
+fs.readFile('./Index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
