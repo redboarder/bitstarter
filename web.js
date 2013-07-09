72988704847512e19bd34e5e48be248b0496b029
Kirill <k.gorbushin@gmail.com>
2013-07-09 23:47:40 +0200

    read index.html

--- web.js
+++ web.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var content;
+var buffer=new Buffer("Hello", "utf-8");
 // First I want to read the file
-fs.readFile('./Index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
+var data = fs.readFileSync('index.html', "utf-8");
+    buffer.write(data,"utf-8");
 
     // Invoke the next step here however you like
-    console.log(content);   // Put all of the code here (not the best solution)
-});
+//    console.log(content);   // Put all of the code here (not the best solution)
+var content = buffer.toString('utf-8');
+console.log(data);
+//}, "utf-8");
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
